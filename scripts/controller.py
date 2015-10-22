6f6f9c56aafcfc601e9007a4278ea3d77dd873af
Naveen Jasty <navjas73@gmail.com>
2015-10-22 16:01:05 -0400

    Need to make waypoint message (in controller), and publish that to command topic

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import rospy
 import time
-from njllrd.proj2.srv import *
-from njllrd.proj2.msg import *
+from njllrd_proj2.srv import *
+from njllrd_proj2.msg import *
 from std_msgs.msg import String
 
 flag = False
 
 def controller():
     rospy.init_node('controller')
-    rospy.Subscriber('user_input', 'std_msgs/String', handle_user_input)
+    rospy.wait_for_service('request_endpoint')
+    rospy.Subscriber('user_input', String, handle_user_input)
     
     connect_points()
 
@@ -17,34 +18,41 @@ def controller():
     # time.sleep(10)
     rospy.spin()
 
-def handle_user_input():
+def handle_user_input(data):
     global flag
     flag = True
+    #print "got here"
 
 
-def request_endpoint():
+def request_position():
+    yes = True
     request = rospy.ServiceProxy('request_endpoint', request_endpoint)
-    return request
+    output = request(yes)
+    return output
 
 def connect_points():
     global flag
 
     #wait for user to hit enter
     while flag == False:
+        x = 1
         #loop
 
-    point = request_endpoint()
+    point = request_position()
     flag = False
 
     while flag == False:
+        x = 1
         #loop
 
     #wait for user to hit enter
-    point2 = request_endpoint()
+    point2 = request_position()
 
     #publish waypoints to robot_interface
     print point
     print point2
+
+    
 
 
 if __name__ == "__main__":
--- scripts/robot_interface.py
+++ scripts/robot_interface.py
@@ -4,10 +4,10 @@ import rospy
 import time
 from std_msgs.msg import String
 import baxter_interface
-import baxter_kinematics
+from baxter_pykdl import baxter_kinematics
 import numpy
-from njllrd.proj2.srv import *
-from njllrd.proj2.msg import *
+from njllrd_proj2.srv import *
+from njllrd_proj2.msg import *
 
 limb = None
 kinematics = None
@@ -16,30 +16,32 @@ tol         = None
 #waypoints = None
 
 def move_to_point(point):
-	# if q_next in reachable_workspace 
-	#while goal is not reached
-	#get x0 from current position
-	#calculate desired velocity   v_des = (x_next - x0)/time_step 
-	# get pseudoinverse
-	# multiply pseudoinverse by desired velocity (J^*v_des) to get joint velocities
-	# set joint velocities with set_joint_velocities
-	x_goal  = [point.x point.y point.z] 
+# if q_next in reachable_workspace 
+#while goal is not reached
+#get x0 from current position
+#calculate desired velocity   v_des = (x_next - x0)/time_step 
+# get pseudoinverse
+# multiply pseudoinverse by desired velocity (J^*v_des) to get joint velocities
+# set joint velocities with set_joint_velocities
+    x_goal  = [point.x, point.y, point.z] 
     at_goal = False
     vel_mag = 0.1
-	while !at_goal:
-		x0   = limb.endpoint_pose()   # current pose
+    while not at_goal:
+        x0   = limb.endpoint_pose()   # current pose
         x0   = x0['position']
-		dist = numpy.linalg.norm(x_goal-x0)
-		if dist < tol:
+        dist = numpy.linalg.norm(x_goal-x0)
+        if dist < tol:
             at_goal = True
-			break
-        else
+            break
+        else:
             # check if x_goal in reachable workspace
             v_des         = [(x_goal - x0)/dist*vel_mag, 0, 0, 0]    # calulate desired velocity, zero angular velocities
-	        J_psuinv      = kinematics.jacobian_pseudo_inverse()
-            q_dot         = j*v_des for j in J_psuinv
-            joint_command = {value:key for key in joint_names for value in q_dot[key]}
-            limb.set_joint_velocities(joint_command)
+            J_psuinv      = kinematics.jacobian_pseudo_inverse()
+            print v_des
+            print J_psuinv
+            #q_dot         = j*v_des for j in J_psuinv
+            #joint_command = {value:key for key in joint_names for value in q_dot[key]}
+            #limb.set_joint_velocities(joint_command)
 
 def move_to_initial_point(point):
     current_pose = limb.endpoint_pose()   # current pose
@@ -55,7 +57,7 @@ def command_handler(data):
     move_to_point(data.points[1])
 
 
-def handle_request_pose(data):
+def handle_request_endpoint(data):
     endpoint = limb.endpoint_pose() # add in offset for tool later
     endpoint_position = point()
     endpoint_position.x = endpoint['position'][0]
@@ -64,32 +66,6 @@ def handle_request_pose(data):
     
     return endpoint_position
 
-def robot_interface():
-    rospy.init_node('robot_interface')
-    
-
-    # Subscribes to waypoints from controller, sent in a set 
-    rospy.Subscriber('command', waypoints, command_handler)
-
-    # Sends message to controller when it's done
-    rospy.Publisher('state', "std_msgs/String")
-
-    # If requested, returns endpoint pose
-    s = rospy.Service('request_pose', request_pose, handle_request_pose)
-
-
-    global limb 
-    global kinematics
-    global joint_names
-    global tol
-    tol         = 0.05
-    joint_names = ['*_s0', '*_s1', '*_e0', '*_e1', '*_w0', '*_w1', '*_w2']
-    limb        = baxter_interface.Limb('left') #instantiate limb
-    kinematics  = baxter_kinematics('left')
-    #global waypoints
-    #waypoints = waypoints()
-    # time.sleep(10)
-    rospy.spin()
 
 def request_kinematics(position, quaternion, side):
     # rospy.init_node("rsdk_ik_service_client")
@@ -123,6 +99,32 @@ def request_kinematics(position, quaternion, side):
     else:
         print("INVALID POSE - No Valid Joint Solution Found.")
     return False
+
+def robot_interface():
+    rospy.init_node('robot_interface')
+    
+
+    # Subscribes to waypoints from controller, sent in a set 
+    rospy.Subscriber('command', waypoints, command_handler)
+
+    # Sends message to controller when it's done
+    rospy.Publisher('state', String, queue_size=10)
+    
+    # If requested, returns endpoint pose
+    s = rospy.Service('request_endpoint', request_endpoint, handle_request_endpoint)
+
+
+    global limb 
+    global kinematics
+    global joint_names
+    global tol
+    tol         = 0.05
+    joint_names = ['*_s0', '*_s1', '*_e0', '*_e1', '*_w0', '*_w1', '*_w2']
+    limb        = baxter_interface.Limb('left') #instantiate limb
+    kinematics  = baxter_kinematics('left')
+    global waypoints
+    waypoints = waypoints()
+    rospy.spin()
 
 
 if __name__ == "__main__":
