4a318265110c46ec1debcd6ae9f91474b59a1b1a
Naveen Jasty <navjas73@gmail.com>
2015-10-25 18:42:48 -0400

    Added request from controller, still getting errors when trying to move

--- scripts/robot_interface.py
+++ scripts/robot_interface.py
@@ -3,17 +3,28 @@
 import rospy
 import time
 from std_msgs.msg import String
+from std_msgs.msg import Header
 import baxter_interface
 from baxter_pykdl import baxter_kinematics
 import numpy
 from njllrd_proj2.srv import *
 from njllrd_proj2.msg import *
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
 
 limb = None
 kinematics = None
 joint_names = None
 tol         = None
-#waypoints = None
+points = None
 
 def move_to_point(point):
 # if q_next in reachable_workspace 
@@ -29,32 +40,46 @@ def move_to_point(point):
     while not at_goal:
         x0   = limb.endpoint_pose()   # current pose
         x0   = x0['position']
-        dist = numpy.linalg.norm(x_goal-x0)
+        x0 = [x0.x, x0.y, x0.z]
+        
+        dist = numpy.linalg.norm(numpy.subtract(x_goal,x0))
         if dist < tol:
             at_goal = True
+            print "within tolerance"
             break
         else:
             # check if x_goal in reachable workspace
-            v_des         = [(x_goal - x0)/dist*vel_mag, 0, 0, 0]    # calulate desired velocity, zero angular velocities
+            v_des         = numpy.subtract(x_goal,x0)/dist*vel_mag  
+            v_des = numpy.append(vdes, [0,0,0])  # calulate desired velocity, zero angular velocities
             J_psuinv      = kinematics.jacobian_pseudo_inverse()
             print v_des
             print J_psuinv
-            #q_dot         = j*v_des for j in J_psuinv
-            #joint_command = {value:key for key in joint_names for value in q_dot[key]}
-            #limb.set_joint_velocities(joint_command)
+            q_dot         = numpy.dot(J_psuinv,v_des)
+            joint_command = {value:key for key in joint_names for value in q_dot[key]}
+            limb.set_joint_velocities(joint_command)
 
 def move_to_initial_point(point):
     current_pose = limb.endpoint_pose()   # current pose
     new_pose     = limb.Point(point.x, point.y, point.z)
+    print "current_pose"
+    print current_pose['orientation']
+    print "new_pose"
+    print new_pose
     joints       = request_kinematics(new_pose, current_pose['orientation'],'left')
+    print "joints"
+    print joints
     limb.move_to_joint_positions(joints)
+    print "moved to initial point"
 
 def command_handler(data):
-    initial_point = data.points[0]
-    waypoints = data.points[1::]
-    
+    initial_point = data.points.points[0]
+    points = data.points.points[1::]
+    print "initial_point"
+    print initial_point
     move_to_initial_point(initial_point)
-    move_to_point(data.points[1])
+    move_to_point(data.points.points[1])
+    print "end of command handler"
+    return True
 
 
 def handle_request_endpoint(data):
@@ -84,7 +109,7 @@ def request_kinematics(position, quaternion, side):
             ),
     }
     ikreq.pose_stamp.append(poses[side])
-    #print ikreq
+    print ikreq
     try:
         rospy.wait_for_service(ns, 5.0)
         resp = iksvc(ikreq)
@@ -103,9 +128,8 @@ def request_kinematics(position, quaternion, side):
 def robot_interface():
     rospy.init_node('robot_interface')
     
-
     # Subscribes to waypoints from controller, sent in a set 
-    rospy.Subscriber('command', waypoints, command_handler)
+    z = rospy.Service('connect_waypoints', connect_waypoints, command_handler)
 
     # Sends message to controller when it's done
     rospy.Publisher('state', String, queue_size=10)
@@ -122,8 +146,8 @@ def robot_interface():
     joint_names = ['*_s0', '*_s1', '*_e0', '*_e1', '*_w0', '*_w1', '*_w2']
     limb        = baxter_interface.Limb('left') #instantiate limb
     kinematics  = baxter_kinematics('left')
-    global waypoints
-    waypoints = waypoints()
+    global points
+    points = waypoints()
     rospy.spin()
 
 
